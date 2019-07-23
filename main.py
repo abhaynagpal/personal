82f4406a7e31eedbeeb5d1e5ec9fd6d852ef4d53
abhaynagpal <40191645+abhaynagpal@users.noreply.github.com>
2019-07-23 16:31:23 +1000

    Update main.py

--- main.py
+++ main.py
@@ -22,10 +22,8 @@ def export_to_table(query,query_job):
 	 QUERY = fd.read()
 	 fd.close()
 	 bq_client = bigquery.Client(project=PROJECTID)
-	 query_job = bq_client.query(
-	     QUERY, project="mpc-dev-459470", location = "australia-southeast1"
-	 ) # API request
-    results = query_job.result()
+	 query_job = bq_client.query(QUERY, project="mpc-dev-459470", location = "australia-southeast1") # API request
+	 results = query_job.result()
 	 return 'OK'
 	 #while not query_job.done():
 	 #   print( "Job {} is currently in state {}".format( query_job.job_id, query_job.state ) )
